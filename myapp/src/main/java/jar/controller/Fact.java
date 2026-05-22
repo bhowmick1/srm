d62db9de94f52313ea55c2be274f4ef71fd11ddc
bhowmick1 <bhowmick1001@gmail.com>
2026-05-22 10:11:01 +0530

    Added sum and factorial APIs

--- myapp/src/main/java/jar/controller/Fact.java
+++ myapp/src/main/java/jar/controller/Fact.java
@@ -1,5 +1,21 @@
 package jar.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Fact {
-    
+
+    @GetMapping("/api/fact")
+    public long factorial(@RequestParam int n) {
+
+        long fact = 1;
+
+        for(int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        return fact;
+    }
 }
--- myapp/src/main/java/jar/controller/Sum.java
+++ myapp/src/main/java/jar/controller/Sum.java
@@ -1,5 +1,17 @@
 package jar.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
 public class Sum {
-    
+
+    @GetMapping("/api/sum")
+    public int sum(@RequestParam int a, @RequestParam int b) {
+
+        return a + b;
+    }
 }
